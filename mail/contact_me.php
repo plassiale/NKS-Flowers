a4033c04984d23729040d155d42907890d85c818
Pierre LASSIALE <pierre.lassiale@gmail.com>
2015-10-26 15:00:28 +0100

    contact form in comming soon page
    contact form updated to

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,35 @@
 <?php
+
+$to = 'contact@nks-flowers.fr';
+$email_subject = "[nks-flowers.fr] Contact site";
+$email_address = $_POST['email'];
+
+//--------
+// from simple email form - news
+//--------
+
+/* subscribe form */
+$subscribe = $_POST['subscribe'];
+if(!empty(($subscribe))
+{
+  if( empty($email_address))
+  {
+  	echo "{'resultat' : 'KO'}";
+  	return false;
+  }
+
+  $email_subject = $email_subject + ' - souscription';
+  $email_body = "Message du site nks-flowers.fr.\n\n"."Nouvelle subscription.\n\nEmail: $email_address";
+  $headers = "From: noreply@nks-flowers.fr\n";
+  $headers .= "Reply-To: $email_address";
+  mail($to,$email_subject,$email_body,$headers);
+  echo "{'resultat' : 'OK'}";
+  return true;
+}
+
+//--------
+// from contact form
+//--------
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -9,13 +40,10 @@ if(empty($_POST['name'])  		||
    }
 
 $name = $_POST['name'];
-$email_address = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'contact@nks-flowers.fr'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "[nks-flowers.fr] Contact site";
 $email_body = "Message du site nks-flowers.fr.\n\n"."Details:\n\nNom: $name\n\nEmail: $email_address\n\nTelephone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@nks-flowers.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";
